16e3fd6f21736b48078e3c000b0f695c8dee7c4b
pickelt <160452501+pickelt@users.noreply.github.com>
2025-09-21 18:39:15 -0400

    Add files via upload

---
+++ P1LAB1_PickelTyler.py
@@ -0,0 +1,11 @@
+# Your Name: Tyler Pickel
+# Date: 09/21/2025
+# Assignment Name: P1LAB1
+# Description: Program that asks for first and last name and prints a welcome message.
+
+# Ask the user for their first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display the welcome message
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
